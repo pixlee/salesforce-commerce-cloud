48074e77c7b40e768179f4839234691ba8cf065e
Yunfan Luo <yunfan@pixleeteam.com>
2016-11-29 19:02:51 -0800

    - Forgot to git rm some js files

--- int_pixlee/cartridge/controllers/sentrylogging.js
+++
@@ -1,9 +0,0 @@
-/**
- * A hello world controller.
- *
- * @module controllers/Hello
- */
-exports.World = function(){
-    response.getWriter().println('Hello World!');
-};
-exports.World.public = true; 
