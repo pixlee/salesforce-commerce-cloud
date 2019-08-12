1df75451c7500b9ac8e648a5303a9d7d5a5b05cc
Ivan Dimitrov <ivan.dimitrov@tryzens.com>
2019-08-12 16:34:00 +0300

    Fixed metadata spelling issues. Fixed all errors after lint run. Removed the pipeline and updated the documentation with the script node which should be added instead of the call node to the legacy pipeline.

--- test/mocks/dw/system/Site.js
+++ test/mocks/dw/system/Site.js
@@ -1,17 +1,18 @@
 'use strict';
 
-module.exports = function(params) {
+module.exports = function (params) {
     var skuReference = params.skuReference;
-    
+
     return {
         current: {
-            getCustomPreferenceValue: function(value) {
-                if (value == 'SkuReference') {
-                    if (skuReference == 'Manufacturer SKU')
+            getCustomPreferenceValue: function (value) {
+                if (value === 'SkuReference') {
+                    if (skuReference === 'Manufacturer SKU') {
                         return 'Manufacturer SKU';
-                    else
-                        return 'Product ID';
+                    }
+                    return 'Product ID';
                 }
+                return null;
             }
         }
     };
--- test/mocks/scripts/pixlee/helpers/pixleeHelper.js
+++ test/mocks/scripts/pixlee/helpers/pixleeHelper.js
@@ -4,7 +4,7 @@ var proxyquire = require('proxyquire').noCallThru().noPreserveCache();
 
 var Site = require('../../../dw/system/Site');
 
-module.exports = function(params) {
+module.exports = function (params) {
     return proxyquire('../../../../../cartridges/int_pixlee_core/cartridge/scripts/pixlee/helpers/pixleeHelper', {
         'dw/system/Site': new Site(params)
     });
